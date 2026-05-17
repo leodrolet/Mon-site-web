1710b9a54d1c879e422e5c048b27e511fc758f41
Theman3tt3-1 <theman3tt3@gmail.com>
2026-05-17 00:59:45 -0400

    feat: orange color scheme + fix process timeline line

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import { About, Services } from './components/AboutServices';
@@ -9,9 +9,22 @@ import Pricing from './components/Pricing';
 import ContactForm from './components/ContactForm';
 import FinalCTA from './components/FinalCTA';
 import Footer from './components/Footer';
+import { smoothScrollTo } from './components/utils';
 import './index.css';
 
 function App() {
+  useEffect(() => {
+    function handleClick(e: MouseEvent) {
+      const target = e.target as HTMLElement;
+      const anchor = target.closest('a[href^="#"]') as HTMLAnchorElement | null;
+      if (!anchor) return;
+      e.preventDefault();
+      smoothScrollTo(anchor.getAttribute('href')!);
+    }
+    document.addEventListener('click', handleClick);
+    return () => document.removeEventListener('click', handleClick);
+  }, []);
+
   return (
     <div className="min-h-screen selection:bg-orange-500 selection:text-white bg-black">
       <Navbar />
