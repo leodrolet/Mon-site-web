519a2128a8fb435d09d12e5a7467f8a1a56eedfc
Theman3tt3-1 <theman3tt3@gmail.com>
2026-05-21 06:51:08 -0400

    feat: orange color scheme + fix process timeline line

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import ContactForm from './components/ContactForm';
 import FinalCTA from './components/FinalCTA';
 import Footer from './components/Footer';
 import { smoothScrollTo } from './components/utils';
-import { BackgroundPaths } from './components/ui/background-paths';
 import './index.css';
 
 function App() {
@@ -28,8 +27,7 @@ function App() {
 
   return (
     <div className="min-h-screen selection:bg-orange-500 selection:text-white bg-black">
-      <BackgroundPaths />
-      <Navbar />
+<Navbar />
       <main>
         <Hero />
         <WhyLandingPage />
